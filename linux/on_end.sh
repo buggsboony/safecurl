be0da987d205fa8660a7108d959fcde4d6e43662
buggsboony <bruno.lesdema@gmail.com>
2021-05-18 12:31:56 -0400

    on_end.sh modified

--- linux/on_end.sh
+++ linux/on_end.sh
@@ -4,21 +4,22 @@
 success="$1"
 curlAct="$2"
 
+title="SafeCurl"
 iconok="/usr/share/themes/Breath/assets/check-checked-hover@2.png"
 iconfail="/usr/share/themes/Breath/assets/titlebutton-close-active-backdrop@2.png"
 
 #aplay "/usr/lib/libreoffice/share/gallery/sounds/left.wav";
-   notify-send "SafeCurl $curlAct SUCCEEDED !";
-notify-send -u critical -i "notification-message-IM" 'Boss !!' 'Am done with the execution'
+#   notify-send "SafeCurl $curlAct SUCCEEDED !";
+#notify-send -u critical -i "notification-message-IM" 'Boss !!' 'Am done with the execution'
 
 if [ $success == "True" ];then
    #echo "succedded $curlAct">/home/boony/safecurl.txt
    #aplay "/usr/lib/libreoffice/share/gallery/sounds/untie.wav";
    #aplay "/usr/lib/libreoffice/share/gallery/sounds/left.wav";
-   notify-send "$title - $curlAct" "SUCCEEDED !" -i "$iconok";
+   notify-send "$title - $curlAct" "SUCCEEDED !" -i "$iconok" --expire-time 500;
 else
    #echo "operation $curlAct failed">/home/boony/safecurl.txt
-   notify-send "$title - $curlAct" "FAILED !" -i "$iconfail";
-   aplay "/usr/lib/libreoffice/share/gallery/sounds/left.wav";   
+   notify-send "$title - $curlAct" "FAILED !" --icon "$iconfail" --expire-time 50000
+      aplay "/usr/lib/libreoffice/share/gallery/sounds/left.wav"
 fi
 
